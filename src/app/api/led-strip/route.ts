3bd72adabd3f3964f409fcba24bfb6962a6ae8af
Stavros <steveiliop56@gmail.com>
2023-11-27 15:09:49 +0200

    feat(routes): finish api routes and python handler

--- src/app/api/led-strip/route.ts
+++ src/app/api/led-strip/route.ts
@@ -1,5 +1,20 @@
+import { exec } from "child_process";
+
 export async function POST(request: Request) {
   const data = await request.json();
-  console.log(data);
-  return Response.json({ hello: true });
+  var command =
+    "python3 python-handler.py pixels " +
+    data["led_req"]["led1"] +
+    data["led_req"]["led2"] +
+    data["led_req"]["led3"] +
+    data["led_req"]["led4"] +
+    data["led_req"]["led5"] +
+    data["led_req"]["led6"] +
+    data["led_req"]["led7"] +
+    data["led_req"]["led8"];
+  command = command.replaceAll("#", " ");
+  exec(command);
+  return new Response("", {
+    status: 200,
+  });
 }
--- src/app/api/sweets-controller/route.ts
+++ src/app/api/sweets-controller/route.ts
@@ -1,17 +1,9 @@
+import { exec } from "child_process";
+
 export async function POST(request: Request) {
   const data = await request.json();
-  console.log(data);
-  return Response.json({ ok: true });
+  exec(`python3 python-handler.py servo`);
+  return new Response("", {
+    status: 200,
+  });
 }
-
-// import { exec } from "child_process"
-
-// export async function POST(request: Request) {
-//     python_handler(Response.json({ request }))
-//     return Response.json({ request })
-// }
-
-// function python_handler (request) {
-//     console.log(request)
-//     exec(`python3 python-handler.py ${request}`)
-// }
