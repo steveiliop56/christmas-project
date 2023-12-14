4a838a4d4f3e59c4eba7d48d078e1dfd982cfdee
Stavros <steveiliop56@gmail.com>
2023-12-14 16:28:02 +0200

    refactor(api): logs on sweets-controller

--- src/app/api/sweets-controller/route.ts
+++ src/app/api/sweets-controller/route.ts
@@ -2,7 +2,17 @@ import { exec } from "child_process";
 
 export async function POST(request: Request) {
   const data = await request.json();
-  exec(`python3 python-handler.py servo`);
+  exec(`python3 python-handler.py servo`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
   return new Response("", {
     status: 200,
   });
