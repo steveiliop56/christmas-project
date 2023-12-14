a83e220d842c7ca4132a8f015f3057140cef6773
Stavros <steveiliop56@gmail.com>
2023-12-14 16:28:36 +0200

    refactor(api): add logs on muic controller

--- src/app/api/music-playing/route.ts
+++ src/app/api/music-playing/route.ts
@@ -3,9 +3,29 @@ import { exec } from "child_process";
 export async function POST(request: Request) {
   const data = await request.json();
   if (data["play"] == true) {
-    exec(`python3 python-handler.py song play ${data["song"]}`);
+    exec(`python3 python-handler.py song play ${data["song"]}`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
   } else {
-    exec(`python3 python-handler.py song stop`);
+    exec(`python3 python-handler.py song stop`, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
   }
   return new Response("", {
     status: 200,
